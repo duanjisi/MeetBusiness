61d9734cdfeccbbcc842f22564405b908255af74
Johnny <576248427@qq.com>
2017-05-12 15:05:49 +0800

    Initial commit

---
+++ app/src/main/java/com/boss66/meetbusiness/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.boss66.meetbusiness.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.boss66.meetbusiness.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
