e379903b350206bd8b2bdd160352a6d11d89f343
duanjisi <576248427@qq.com>
2017-07-06 15:04:08 +0800

    MVP-Rxjava-retrofit 引入

---
+++ app/src/main/java/com/boss66/meetbusiness/retrofit/ApiStores.java
@@ -0,0 +1,21 @@
+package com.boss66.meetbusiness.retrofit;
+
+
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+/**
+ *  Created by liw on 2017/7/5.
+ */
+public interface ApiStores {
+    //baseUrl
+    String API_SERVER_URL = "http://www.weather.com.cn/";
+
+
+
+    //加载天气
+    @GET("adat/sk/{cityId}.html")
+    Observable<String> loadDataByRetrofitRxjava(@Path("cityId") String cityId);
+}
